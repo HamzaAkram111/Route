bb6d839005ebeb2465c49e924992cc24c073baba
HamzaAkram111 <zafikhanzafi007@gmail.com>
2025-04-11 07:49:37 +0000

    ok

---
+++ vite-project/src/Components/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <>
+     <h1>
+        This is Mainn Page
+     </h1>
+      Say Hello<input type='text'>
+
+     </input>
+
+
+
+    </>
+
+  )
+}
+
+export default Main
